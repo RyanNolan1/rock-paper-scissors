101d7bddb7f98dbcc1901c813ca7784a41885f52
RyanNolan1 <114003965+RyanNolan1@users.noreply.github.com>
2023-04-28 17:57:11 +0100

    this will make the correct hand shows when the buttons are clicked for the user

--- index.js
+++ index.js
@@ -7,10 +7,21 @@ document.addEventListener("DOMContentLoaded", function () {
   const buttons = document.querySelectorAll(".buttons");
   buttons.forEach(function (currentButton) {
     currentButton.addEventListener("click", function () {
+      getHandImages(currentButton.value);
       console.log(playRound(currentButton.value, getComputerChoice()));
     });
   });
 });
+
+function getHandImages(handValue) {
+  if (handValue === "Rock") {
+    document.getElementById("user-hand").src ="./images/rock.jpg";
+  } else if (handValue === "Paper") {
+    document.getElementById("user-hand").src ="./images/paper.jpg";
+  } else if (handValue === "Scissors") {
+    document.getElementById("user-hand").src ="./images/scissors.jpg";
+  }
+}
 
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === "Rock" && computerSelection === "Scissors") {
