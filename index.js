a52bd2f8ad62bbd0e88fd0fa1f7eb7b1af68c41f
RyanNolan1 <114003965+RyanNolan1@users.noreply.github.com>
2023-04-28 14:02:26 +0100

    this will return the result of a round if the player uses rock

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 
 function getComputerChoice() {
     const choiceArray = ["Rock", "Paper", "Scissors"]
-    console.log(choiceArray[Math.floor(Math.random() * 3)])
+    return choiceArray[Math.floor(Math.random() * 3)]
 }
 
-getComputerChoice()
+function playRound (playerSelection, computerSelection) {
+    console.log(computerSelection + " Computers selection")
+
+    if ( playerSelection === 'Rock' && computerSelection === 'Scissors' ) {
+        return 'You Win! Rock beats Scissors'
+
+    } else if ( playerSelection === 'Rock' && computerSelection === 'Rock' ) {
+        return 'Draw! Rock draws with Rock'
+
+    } else if ( playerSelection === 'Rock' && computerSelection === 'Paper' ) {
+        return 'You Lose! Paper beats Rock'
+}
+}
+
+const playerSelection = 'Rock';
+const computerSelection = getComputerChoice();
+
+
+console.log(playRound(playerSelection, computerSelection));
